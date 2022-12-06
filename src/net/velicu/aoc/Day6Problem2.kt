29e87c58333911e0176744cf3e000d2f032640b5
Adi Velicu <adi.velicu@gmail.com>
2022-12-05 23:08:12 -0800

    be a bit more efficient at the expense of writing a ton more code

--- src/net/velicu/aoc/Day6Problem2.kt
+++ src/net/velicu/aoc/Day6Problem2.kt
@@ -7,17 +7,51 @@ class Day6Problem2 {
 
     const val MARKER_LEN = 14
 
+    private class Multiset<T> {
+      private val map: MutableMap<T, Int> = mutableMapOf()
+
+      fun add(v: T) {
+        map[v] = map.getOrDefault(v, 0).inc()
+      }
+
+      fun remove(v: T) {
+        map.getOrDefault(v, 0).dec().let {
+          if (it == 0) {
+            map.remove(v)
+          } else {
+            map[v] = it
+          }
+        }
+      }
+
+      fun asSet() : Set<T> {
+        return map.keys
+      }
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
       val message = File("inputs/6.input.txt").readText()
-      val firstMarkerPos =
-        message
-          .windowed(MARKER_LEN)
-          .map { it.toSet().size == MARKER_LEN }
-          .withIndex()
-          .filter { (_, chunkStart) -> chunkStart }
-          .minOf { (index, _) -> index }
-      println(firstMarkerPos + MARKER_LEN)
+      val runningSet = Multiset<Char>()
+      val deque = ArrayDeque<Char>()
+
+      val firstMarker = run {
+        for ((idx, c) in message.withIndex()) {
+          runningSet.add(c)
+          deque.add(c)
+
+          if (deque.size > MARKER_LEN) {
+            val expunged = deque.removeFirst()
+            runningSet.remove(expunged)
+          }
+
+          if (deque.size == MARKER_LEN && runningSet.asSet().size == MARKER_LEN) {
+            return@run idx + 1
+          }
+        }
+        return@run null
+      }
+      println(firstMarker)
     }
   }
 }
